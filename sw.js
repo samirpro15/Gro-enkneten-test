7e9e58ee7ff23a61c2e48b91042ba1a78ee63475
samirpro15 <70219008+samirpro15@users.noreply.github.com>
2026-09-20 18:23:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // ist und offline zumindest ihr Grundgerüst anzeigt. status.json wird
 // bewusst NICHT gecacht, damit immer der aktuelle Stand angezeigt wird.
 
-const CACHE_NAME = 'terminradar-shell-v5';
+const CACHE_NAME = 'terminradar-shell-v7';
 const SHELL_FILES = [
   './',
   './index.html',
